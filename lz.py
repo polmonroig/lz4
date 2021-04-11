185411dc73c40e26e8f7b072655c09246e58bf32
Pol Monroig <hello@polmonroig.com>
2021-04-11 09:13:39 +0200

    Add complete lz4 to test

--- lz.py
+++ lz.py
@@ -1,29 +1,49 @@
-from lz4 import LZ4
+import lz4.frame as frame
 import sys
 
+
+
+class LZ4:
+
+    ENCODE_EXT = '.lz4'
+    DECODE_EXT = '_decoded'
+
+    def compress(self, text):
+        return text
+
+    def decompress(self, code):
+        return code
 
 
 
 
 def main():
     # create instance on encoder
-    encoder = LZ4()
+    #encoder = LZ4()
+    encoder = frame
     # if we don't have enough argumemts return
     if len(sys.argv) < 3:
         print('Not enough arguments provided')
     # if we want to compress we read the specific file
     elif sys.argv[1] == '-c':
         file = sys.argv[2]
-        fd = open(file, 'r')
+        fd = open(file, 'rb')
         print('Compressing file', file)
-        code = encoder.encode(fd.read())
+        # read file and encode
+        code = encoder.compress(fd.read())
+        # create new file
+        with open(file + LZ4.ENCODE_EXT, 'wb') as out:
+            out.write(code)
         fd.close()
     # if we want to decompress we read the specific file
     elif sys.argv[1] == '-d':
         file = sys.argv[2]
-        fd = open(file, 'r')
+        fd = open(file, 'rb')
         print('Decompressing file', file)
-        text = encoder.decode(fd.read())
+        text = encoder.decompress(fd.read())
+        # create new file
+        with open(file.split('.')[0] + LZ4.DECODE_EXT, 'wb') as out:
+            out.write(text)
         fd.close()
     # if the command specified is unknown skip
     else:
