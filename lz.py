940d4fb54c5fd06307b5a0f80bd00488c33dd3a7
Pol Monroig <hello@polmonroig.com>
2021-04-25 13:34:20 +0000

    Updates

--- lz.py
+++ lz.py
@@ -22,80 +22,92 @@ class LZ4:
 
     def readToken(self, code):
         self.literalLength = code[self.it] >> 4 # 4 highest bits
-        self.matchLength = (code[self.it] % 16) + LZ4.MIN_MATCH_LENGTH
-        print('Literal length:', self.literalLength)
-        print('Match length:', self.matchLength)
+        self.matchLength = (code[self.it] % 16)
+        
         self.it += 1
 
     def readLiteral(self, code):
-        literal = code[self.it:self.it + self.literalLength].decode('utf-8')
+        literal = code[self.it:self.it + self.literalLength]
         self.it += self.literalLength
-        print('Literal found:', literal)
+        # print('Literal found:', literal)
         return literal
 
     def readLiteralLenght(self, code):
         self.literalLength = self.readLSIC(code, self.literalLength)
-
+        # print('Literal length:', self.literalLength)
+        
 
     def readMatchLength(self, code):
-        self.matchLength = self.readLSIC(code, self.matchLength)
+        self.matchLength = self.readLSIC(code, self.matchLength) + LZ4.MIN_MATCH_LENGTH
+        # print('Match length:', self.matchLength)
 
     def readOffset(self, code):
         higher = code[self.it + 1]
         lower = code[self.it]
-        self.offset =  (higher << 4) + lower
+        # print('Offset hex:', code[self.it:self.it + 1])
+        self.offset = (higher << 8) + lower
         self.it += 2
-        print('Offset:', self.offset)
+        # print('Offset:', self.offset)
 
     def readLSIC(self, code, initialLength):
         length = initialLength
         currentByte = initialLength
         # 15 == 4 bits
         if currentByte >= 15:
-            self.it += 1
             currentByte = code[self.it]
             # 8 bits
             while currentByte >= 255:
                 length += currentByte
                 self.it += 1
                 currentByte = code[self.it]
+            
             length += currentByte
             self.it += 1 # next block
 
         return length
 
     def readMatch(self, text):
+        # print('Text Length:', len(text))
+        # print('Offset:', self.offset)
         pos = len(text) - self.offset
         for i in range(self.matchLength):
-            text += text[pos + i]
+            text += text[pos + i].to_bytes(1, 'big') 
+        
 
         return text
 
 
     def decompress(self, code):
         self.it = 0
-        text = ""
-        while self.it < len(code):
-            self.readToken(code)
-            print('It:', self.it)
-            self.readLiteralLenght(code)
-            print('It:', self.it)
-            literal = self.readLiteral(code)
-            print('It:', self.it)
-            text += literal
-            if self.it < len(code): # in case it is the last token
-                self.readOffset(code)
-                print('It:', self.it)
-                self.readMatchLength(code)
-                print('It:', self.it)
-                # add
-                text = self.readMatch(text)
-                print('It:', self.it)
-
-            print('Text:', text)
+        text = b""
+        with tqdm(total=len(code)) as pbar:
+            it_old = self.it 
+            while self.it < len(code):
+                
+                pbar.update(self.it - it_old)
+                it_old = self.it
+                self.readToken(code)
+                #print('It:', self.it)
+                self.readLiteralLenght(code)
+                #print('It:', self.it)
+                literal = self.readLiteral(code)
+                #print('It:', self.it)
+                text += literal
+                if self.it < len(code): # in case it is the last token
+                    self.readOffset(code)
+                    #print('It:', self.it)
+                    self.readMatchLength(code)
+                    #print('It:', self.it)
+                    # add
+                    text = self.readMatch(text)
+                    #print('It:', self.it)
+                # print(offsets[k], "==", self.offset)
+                # print('Block:', code[it_old:self.it])
+                # print('Text:', text)
 
 
         return text
+
 
 
 
