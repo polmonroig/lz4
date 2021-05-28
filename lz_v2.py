5d8d9b90ed764252d2d13c13a286484b0af72cd1
Pol Monroig <hello@polmonroig.com>
2021-05-28 20:00:04 +0200

    Increase decompression speed

--- lz_v2.py
+++ lz_v2.py
@@ -162,7 +162,7 @@ class LZ4:
             distance = initialLength - pos
             # preacollate
             out +=  b"0" * length
-            if True:
+            if distance < self.matchLength:
                 for i in range(length):
                     out[initialLength + i] = out[pos + i]
             else:
